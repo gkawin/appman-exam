3e121fea10210414e5472ed4cdf5714430e9caab
Kawin Glomjai <g.kawin@live.com>
2018-05-25 18:57:21 +0700

    center

--- web/App.js
+++ web/App.js
@@ -1,9 +1,29 @@
+import './styles/app.scss'
+
 import React from 'react'
 
 class App extends React.Component {
   render () {
-    // start your code here
-    return <h1>hello world</h1>
+    return (
+      <div className='app__segment'>
+        <div className='app__logo'>
+          <image src={require('./logo.svg')} />
+        </div>
+        <div className='app__form'>
+          <div className='form__inputs'>
+            <form>
+              <input type='email' placeholder='example@appman.co.th' />
+              <input type='password' placeholder='you password...' />
+              <button onClick={() => {}}>Sign in</button>
+            </form>
+          </div>
+        </div>
+        <div className='form__helpers'>
+          <a href='#'>Forgot password?</a>
+          <a href='#' >Create a new account</a>
+        </div>
+      </div>
+    )
   }
 }
 
